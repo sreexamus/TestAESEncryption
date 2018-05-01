f451153fb011be0cd270e7a527145b0a7cfd3dc2
Sreekanth Reddy <sreekanth.iragamreddy@harman.com>
2018-05-01 12:42:31 -0700

    Initial Commit

---
+++ TestAESEncryption/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestAESEncryption
+//
+//  Created by Sreekanth Iragam Reddy on 5/1/18.
+//  Copyright © 2018 Sreekanth Iragam Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
